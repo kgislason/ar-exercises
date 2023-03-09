826c39c85d59328a4cc686dcdcac459dd92c5b70
Kristy Gislason <kristygislason@gmail.com>
2023-03-09 11:16:24 -0800

    Remvoe comments from Exercise 6

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -11,17 +11,6 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
-
-### Exercise 6: One-to-many association
-
-# We haven't used the Employee class (and employees table) at all yet. If you look at this table's column structure, you'll find that it has a `store_id` (integer) column. This is a column that identifies which store each employee belongs to. It points to the `id` (integer) column of their store.
-
-# Let's tell Active Record that these two tables are in fact related via the `store_id` column.
-
-# 1. Add the following code _directly_ inside the Store model (class): `has_many :employees`
-# 2. Add the following code directly inside the Employee model (class): `belongs_to :store`
-# 3. Add some data into employees. Here's an example of one (note how it differs from how you create stores): `@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)`
-
 @store1.employees.create(first_name: "Gandalf", last_name: "Grey", hourly_rate: 200)
 
 @store1.employees.create(first_name: "Bilbo", last_name: "Baggins", hourly_rate: 30)
