59209703d26b7c927aaf68f55dc3f1ac9880ba9b
Kristy Gislason <kristygislason@gmail.com>
2023-03-09 10:26:04 -0800

    Exercise 5 - get sum of annual revenue for all stores, average annual revenue and count of store where annual revenue is over M

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -4,7 +4,14 @@ require_relative './exercise_2'
 require_relative './exercise_3'
 require_relative './exercise_4'
 
+puts "//////////"
+puts "----------"
 puts "Exercise 5"
 puts "----------"
 
 # Your code goes here ...
+puts "Total revenue for the entire company (all stores): $#{Store.sum(:annual_revenue)}"
+
+puts "Average annual revenue for all stores: $#{Store.average(:annual_revenue)}"
+
+puts "Stores generating $1M or more in annual sales: #{Store.where("annual_revenue >= 1000000").count}"
