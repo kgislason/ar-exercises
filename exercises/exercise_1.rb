67feceff35827d3c30c3e1a9a8b479a65faef6f2
Kristy Gislason <kristygislason@gmail.com>
2023-03-09 10:17:43 -0800

    Add exercise 1 - create 3 new stores and show store count

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -5,7 +5,7 @@ puts "----------"
 
 # Your code goes below here ...
 Store.create(name: "Burnaby", annual_revenue: 300000.00, womens_apparel: true, mens_apparel: true)
-Store.create(name: "Burnaby", annual_revenue: 1260000.00, mens_apparel: true)
+Store.create(name: "Richmond", annual_revenue: 1260000.00, mens_apparel: true)
 Store.create(name: "Gastown", annual_revenue: 190000.00, womens_apparel: true)
 
 puts "Store count: #{Store.all.count}"
