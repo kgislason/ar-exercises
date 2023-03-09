8f52980de044a2cd9d0142d651d0f4f498e9fab6
Kristy Gislason <kristygislason@gmail.com>
2023-03-09 10:10:35 -0800

    Add exercise 2 - assign stores to variables using find_by and then rename one store

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -5,3 +5,12 @@ puts "Exercise 2"
 puts "----------"
 
 # Your code goes here ...
+@store1 = Store.find_by(id: 1)
+@store2 = Store.find_by("id = 2")
+@store3 = Store.find_by(id: 3)
+
+puts "Store 1: #{@store1.name}"
+puts "Store 2: #{@store2.name}"
+
+@store1.name = "Burnaby is Awesome"
+puts "Give store 1 a new name: #{@store1.name}"
